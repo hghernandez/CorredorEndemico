e7bf2bcc5d507df00109473731d5b373e199ac8c
hghernandez <hernanghernandez@gmail.com>
2023-11-09 21:39:40 -0300

    actualizo repo

--- creadataset.R
+++ creadataset.R
@@ -90,5 +90,60 @@ all_week <- data.frame(anos,semanas_epi)
 #Joineo los casos de dengue por semana epidemiologica con el total de semanas
 #por año
 
-all_week %>%
-  left_join(casos_dengue, by= c("anos"="ano","semanas_epi"="semanas_epidemiologicas"))
+casos_dengue <- all_week %>%
+  left_join(casos_dengue, by= c("anos"="ano","semanas_epi"="semanas_epidemiologicas")) %>%
+  replace_na(list(casos=0))
+
+#Hacemos los cálculos
+
+#Creamos el data frame con las poblaciones
+
+anios <- seq(2018,2022)
+poblacion <- c(44494502,44938712,45376763,45808747,46234830)
+
+poblacion <- data.frame(anios,poblacion)
+
+alfa=0.05
+esta$lnICinf=esta$lnmedia-qt(1-alfa/2,esta$n[1])*esta$lnsd/sqrt(esta$n[1])
+
+
+128*1000000/44938712
+
+media_semanal <- casos_dengue %>%
+  left_join(poblacion, by= c("anos"="anios")) %>%
+  mutate(tasa= round((casos/poblacion*1000000+1),4),
+         lntasa= round(log(tasa),4)) %>%
+  group_by(semanas_epi)%>%
+  summarise(lntasa_media= mean(lntasa),
+         sd= sd(lntasa),
+         n= n(),
+         IC_INF= lntasa_media-(qt(1-alfa/2,n)*sd/sqrt(n)),
+         IC_SUP= lntasa_media+(qt(1-alfa/2,n)*sd/sqrt(n)))
+
+#Vuelvo a obtener los valores reales - 1
+
+corredor_dengue <- media_semanal %>%
+  mutate(media= exp(lntasa_media)-1,
+         ic_inf= ifelse(exp(IC_INF)-1<0,0,exp(IC_INF)-1),
+         ic_sup= exp(IC_SUP)-1,
+         media_casos= media*poblacion[5,2]/100000,
+         ic_inf_casos= ifelse(ic_inf*poblacion[5,2]/100000 < 0,0,ic_inf*poblacion[5,2]/100000),
+         ic_sup_casos= ic_sup*poblacion[5,2]/100000,
+         media_to_inf = media_casos-ic_inf_casos,
+         media_to_sup= ic_sup_casos-media_casos)
+
+
+
+ggplot(corredor_dengue,aes(as.numeric(semanas_epi),colour="gold"))+
+  geom_area(aes(,ic_sup_casos),color="sky blue",position = 'stack',fill = I("sky blue"))+
+  geom_area(aes(,media_casos),color="gold",position = 'stack',fill = I("gold"))+
+  geom_area(aes(,media_to_inf),color="green",position = 'stack',fill = I("green"))+
+  geom_bar(aes(,media),color="black",position = 'stack',fill = I("blue"),stat = "identity")+
+  theme(
+    panel.grid.major = element_line(colour = "red"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "red")
+  )+
+  ggtitle("Corredor endémico Dengue. Argentina 2018-2022")+ 
+  xlab("Semanas Epidemiológicas")+ ylab("Casos")
+
