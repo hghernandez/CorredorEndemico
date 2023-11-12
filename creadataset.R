f9d93b30aa583790743c7f5bba6d273f105ba393
hghernandez <hernanghernandez@gmail.com>
2023-11-12 18:18:42 -0300

    actualizo repo

--- creadataset.R
+++ creadataset.R
@@ -176,36 +176,41 @@ plotly::ggplotly(graf_casos)
 #                                                          #
 ##%######################################################%##
 
-eti <- list()
+ira <- list()
 
-eti[[1]] <- read.delim("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/f4096f8b-1692-4d5f-a6d8-09cae47931a4/download/vigilancia-respiratorias-agudas-2018-hasta-20200106.csv",
+ira[[1]] <- read.delim("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/f4096f8b-1692-4d5f-a6d8-09cae47931a4/download/vigilancia-respiratorias-agudas-2018-hasta-20200106.csv",
                           header = TRUE,sep=",")
 
-eti[[1]] <- eti[[1]] %>% filter(anio %in% c(2018,2019))
 
-eti[[2]] <- openxlsx::read.xlsx("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/cda1ba2a-955a-42b0-9d45-8ce4439d2106/download/informacion-publica-respiratorias-nacional-hasta-20220309.xlsx")
+ira[[1]] <- ira[[1]] %>% filter(anio %in% c(2018,2019)) %>%
+  rename("departamento_id"= ï..departamento_id)
 
-eti[[2]] <- eti[[2]] %>% filter(año %in% c(2020,2021)) %>%
+
+
+ira[[2]] <- openxlsx::read.xlsx("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/cda1ba2a-955a-42b0-9d45-8ce4439d2106/download/informacion-publica-respiratorias-nacional-hasta-20220309.xlsx")
+
+ira[[2]] <- ira[[2]] %>% filter(año %in% c(2020,2021)) %>%
   rename("anio"= año)
 
 
-eti[[3]] <-openxlsx::read.xlsx("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/37feeed4-dfcd-4f39-a403-93a6b4bd90d2/download/informacion-publica-respiratorias-nacional-hasta-20230706.xlsx")
+ira[[3]] <-openxlsx::read.xlsx("http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/37feeed4-dfcd-4f39-a403-93a6b4bd90d2/download/informacion-publica-respiratorias-nacional-hasta-20230706.xlsx")
 
-eti[[3]] <- eti[[3]] %>% rename("anio"= año)
+ira[[3]] <- ira[[3]] %>% rename("anio"= año)
 
 
 #Unifico el dataframe
 
-eti_df <- do.call(rbind,eti)
+ira_df <- do.call(rbind,ira)
 
-casos_eti <- eti_df %>%
+
+casos_ira <- ira_df %>%
   group_by(anio,semanas_epidemiologicas)%>%
   summarise(casos= sum(cantidad_casos))
 
 
 #Reasingo casos para la semana 1 a 17 de 2018
 
-compl_2018 = eti_df %>%
+compl_2018 = ira_df %>%
   filter(semanas_epidemiologicas >= 1 & semanas_epidemiologicas <= 17 & anio > 2018 & anio < 2023)%>%
   group_by(anio,semanas_epidemiologicas) %>%
   summarise(casos= sum(cantidad_casos)) %>%
@@ -217,11 +222,11 @@ compl_2018 = eti_df %>%
 
 #Unifico todos
 
-casos_eti <- rbind(compl_2018,
-                   casos_eti)
+casos_ira <- rbind(compl_2018,
+                   casos_ira)
 
-save(casos_eti,file= "casos_eti.RData")
-load("casos_eti.RData")
+save(casos_ira,file= "casos_ira.RData")
+load("casos_ira.RData")
 #Creamos el data frame con las poblaciones
 
 anios <- seq(2018,2022)
@@ -234,7 +239,7 @@ alfa=0.05
 
 #Calculo la media semanal ETI
 
-media_semanal_eti <- casos_eti %>%
+media_semanal_ira <- casos_ira %>%
   filter(anio < 2023) %>%
   left_join(poblacion, by= c("anio"="anios")) %>%
   mutate(tasa= round((casos/poblacion*100000+1),4),
@@ -249,7 +254,7 @@ media_semanal_eti <- casos_eti %>%
 
 #Vuelvo a obtener los valores reales - 1
 
-corredor_eti <- media_semanal_eti %>%
+corredor_ira <- media_semanal_eti %>%
   mutate(media= exp(lntasa_media)-1,
          ic_inf= ifelse(exp(IC_INF)-1<0,0,exp(IC_INF)-1),
          ic_sup= exp(IC_SUP)-1,
@@ -262,18 +267,18 @@ corredor_eti <- media_semanal_eti %>%
 
 #Armo el gráfico
 
-ggplot(corredor_eti,aes(x = semanas_epidemiologicas#, fill=key
+ggplot(corredor_ira,aes(x = semanas_epidemiologicas#, fill=key
 )) +
   geom_area(aes(x=semanas_epidemiologicas, y=new), fill = "#981000", alpha=0.6, stat="identity")+
   geom_area(aes(x=semanas_epidemiologicas, y= ic_sup_casos), fill = "#fee76a", stat="identity")+
   geom_area(aes(x=semanas_epidemiologicas, y= media_casos), fill = "#3e9e39",  stat="identity")+
   geom_area(aes(x=semanas_epidemiologicas, y= ic_inf_casos), fill = "white", stat="identity") +
-  geom_line(data= casos_eti %>% filter(anio== 2023 & semanas_epidemiologicas <= 23),mapping = aes(x=semanas_epidemiologicas,y= casos, colour= "casos"), size=0.8)+
+  geom_line(data= casos_ira %>% filter(anio== 2023 & semanas_epidemiologicas <= 23),mapping = aes(x=semanas_epidemiologicas,y= casos, colour= "casos"), size=0.8)+
   theme_bw()+
   scale_x_continuous(breaks=seq(1,53, 1))+
   scale_color_manual(name="Casos 2023", values= c(casos="black"))+
   scale_y_continuous(label= scales::label_number_si())+
-  labs(title= "Corredor endémico ETI. Argentina 2018-2022",
+  labs(title= "Corredor endémico IRA. Argentina 2018-2022",
        x= "Semanas Epidemiológicas",y="Casos")
 
 
@@ -284,13 +289,13 @@ RColorBrewer::brewer.pal.info
 RColorBrewer::brewer.pal(8,"Set1")
 
 
-casos_eti %>%
+casos_ira %>%
   filter(anio < 2023) %>%
   ggplot(aes(x=semanas_epidemiologicas, y=casos, color=as.factor(anio),linetype=as.factor(anio),group= anio))+
   geom_line(size=0.9)+
-  geom_line(data= casos_eti %>% filter(anio== 2023 & semanas_epidemiologicas <26),
+  geom_line(data= casos_ira %>% filter(anio== 2023 & semanas_epidemiologicas <26),
             mapping = aes(x=semanas_epidemiologicas, y=casos, color=as.factor(anio),linetype=as.factor(anio)))+
-  labs(title= "Curvas epidémicas ETI. Argentina 2018-2022",
+  labs(title= "Curvas epidémicas IRA. Argentina 2018-2022",
        x= "Semanas Epidemiológicas",y="Casos", color= "Años",linetype= "Años")+
   scale_color_manual(values= c("#E41A1C","#377EB8","#4DAF4A","#984EA3","#FF7F00","#F781BF"))+
   scale_x_continuous(breaks=seq(1,53, 1))+
